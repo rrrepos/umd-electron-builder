9b5a5a4e247880a5589fbb9b47199ba8d2fc75df
RmR <connect2rmr@gmail.com>
2021-02-12 16:40:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow } = require('electron');
+var fs = require('fs');
+
+async function openFile(filepath) {
+  let arraybuffer;
+
+  if (filepath) {
+    try {
+      arraybuffer = await fs.readFileSync(filepath);
+      mainWindow.webContents.send("fileinfo", { "filepath": filepath, "buffer": arraybuffer });
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+function createWindow() {
+  return new Promise(resolve => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    center: true,
+    backgroundColor: "#1e1e1e",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.setMenu(null);
+
+  win.loadURL("https://rrrepos.github.io/umd-project-org/electron");
+  win.webContents.on('did-finish-load', () => {
+    resolve(win);
+  });
+});
+}
+
+let mainWindow = null;
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus();
+      if(commandLine[1]) {
+        openFile(commandLine[1]);
+      }
+    }
+  });
+
+  app.whenReady()
+    .then(_ => {
+      return createWindow();
+    }).then(_win => {
+      mainWindow = _win;
+      if (process.argv[1]) {
+        openFile(process.argv[1]);
+      }
+    });
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = createWindow()
+  }
+});
+
