cd4d9f10745b54a2f33b33f339f9e94e7fd82d3b
RmR <connect2rmr@gmail.com>
2021-02-13 13:02:21 +0530

    changed package.json

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 var fs = require('fs');
-const log = require('electron-log');
 
 async function openFile(filepath) {
-  log.info("openfile");
   let arraybuffer;
 
   if (filepath) {
@@ -18,7 +16,6 @@ async function openFile(filepath) {
 }
 
 function createWindow() {
-  log.info("createwindow");
   return new Promise(resolve => {
     const win = new BrowserWindow({
       width: 800,
@@ -39,17 +36,14 @@ function createWindow() {
 }
 
 let mainWindow = null;  // window object
-let appLaunched = false; // status if app did finish loading
-let filepath = null; // stores filepath if open-file event launched before did finish loading
+let filepath = null; // stores filepath if open-file event launched 
 
 const gotTheLock = app.requestSingleInstanceLock();
 
 if (!gotTheLock) {
-  log.info("app quit");
   app.quit()
 } else {
   app.on('second-instance', (event, commandLine, workingDirectory) => {
-    log.info("second-instance");
     if (mainWindow) {
       if (mainWindow.isMinimized()) mainWindow.restore()
       mainWindow.focus();
@@ -61,15 +55,9 @@ if (!gotTheLock) {
 
   app.whenReady()
     .then(_ => {
-      log.info("whenReady");
       return createWindow();
     }).then(_win => {
       mainWindow = _win;
-      log.info("whenReady:0", process.argv[0]);
-      log.info("whenReady:1", process.argv[1]);
-      //if (process.argv[1]&&process.argv[1].indexOf('-')==0) process.argv.unshift('');
-      //log.info("whenReady:1", process.argv[1]);
-      log.info("whenReady:2", filepath);
       if(filepath) {
         openFile(filepath);
         filepath = null;
@@ -80,13 +68,8 @@ if (!gotTheLock) {
     });
 
   app.on('will-finish-launching', function () {
-    log.info("will-finish-launching");
-    appLaunched = true;
-
     app.on("open-file", (event, path) => {
-      log.info("open-file");
       if (mainWindow) {
-        log.info("openfile with window")
         if (mainWindow.isMinimized()) mainWindow.restore()
         mainWindow.focus();
         if (path) {
@@ -94,7 +77,6 @@ if (!gotTheLock) {
         }
       }
       else {
-        log.info("open-file without window");
         filepath = path;
       }
     });  
