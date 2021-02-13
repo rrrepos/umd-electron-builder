50ba610a8959f504ba80a3eec9c5e7241baa49ee
RmR <connect2rmr@gmail.com>
2021-02-13 11:51:41 +0530

    cleaned open-file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require('electron');
 var fs = require('fs');
-const log = require('electron-log');
 
 async function openFile(filepath) {
   let arraybuffer;
@@ -18,37 +17,35 @@ async function openFile(filepath) {
 
 function createWindow() {
   return new Promise(resolve => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    center: true,
-    backgroundColor: "#1e1e1e",
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  win.setMenu(null);
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      center: true,
+      backgroundColor: "#1e1e1e",
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+    win.setMenu(null);
 
-  win.loadURL("https://rrrepos.github.io/umd-project-org/electron");
-  win.webContents.on('did-finish-load', () => {
-    resolve(win);
+    win.loadURL("https://rrrepos.github.io/umd-project-org/electron");
+    win.webContents.on('did-finish-load', () => {
+      resolve(win);
+    });
   });
-});
 }
 
 let mainWindow = null;
 const gotTheLock = app.requestSingleInstanceLock();
 
 if (!gotTheLock) {
-  log.info("already open");
   app.quit()
 } else {
   app.on('second-instance', (event, commandLine, workingDirectory) => {
-    log.info('2:' + commandLine);
     if (mainWindow) {
       if (mainWindow.isMinimized()) mainWindow.restore()
       mainWindow.focus();
-      if(commandLine[1]) {
+      if (commandLine[1]) {
         openFile(commandLine[1]);
       }
     }
@@ -59,28 +56,22 @@ if (!gotTheLock) {
       return createWindow();
     }).then(_win => {
       mainWindow = _win;
-      log.info('1:' + process.argv);
       if (process.argv[1]) {
         openFile(process.argv[1]);
       }
     });
 
- app.on("open-url", (event, url) => {
-  log.info("open-url", url);
- });
- 
- app.on("open-file", (event, filepath) => {
-  log.info("open-file", filepath);
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) mainWindow.restore()
-    mainWindow.focus();
-    if(filepath) {
-      openFile(filepath);
-    }
-  }
-
-  openFile(path);
- });    
+  app.on('will-finish-launching', function () {
+    app.on("open-file", (event, filepath) => {
+      if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore()
+        mainWindow.focus();
+        if (filepath) {
+          openFile(filepath);
+        }
+      }
+    });
+  });
 }
 
 app.on('window-all-closed', () => {
@@ -94,4 +85,3 @@ app.on('activate', () => {
     mainWindow = createWindow()
   }
 });
-
